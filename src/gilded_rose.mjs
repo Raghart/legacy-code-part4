3ea2bc59db1e610d881fdc5d0e32b295336bb643
Rafael <rafaelrojas36@outlook.com>
2025-09-13 19:00:11 -0400

    r adding some method to facilitate testing

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -36,14 +36,15 @@ export class Shop {
       }
     }
   }
-
-  updateQuality() {
-    for (var i = 0; i < this.items.length; i++) {
-      this.chocolateSauce(i);
-      if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+  
+  chocolateCream(i) {
+    if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
-      if (this.items[i].sellIn < 0) {
+  }
+
+  chocolatePatatas(i) {
+    if (this.items[i].sellIn < 0) {
         if (this.items[i].name != "Aged Brie") {
           if (this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
             if (this.items[i].quality > 0) {
@@ -60,6 +61,13 @@ export class Shop {
           }
         }
       }
+  }
+
+  updateQuality() {
+    for (var i = 0; i < this.items.length; i++) {
+      this.chocolateSauce(i);
+      this.chocolateCream(i);
+      this.chocolatePatatas(i);
     }
 
     return this.items;
