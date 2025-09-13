efc8b7acc1ec854ee3c369c941e2b6a1e0fc7433
Rafael <rafaelrojas36@outlook.com>
2025-09-13 17:21:52 -0400

    t change var

--- test/gilded_rose.test.mjs
+++ test/gilded_rose.test.mjs
@@ -4,7 +4,8 @@ import { Item, Shop } from "../src/gilded_rose.mjs";
 
 describe("Gilded Rose", () => {
   test("foo", () => {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
+    const shopItems = [new Item("foo", 0, 0)];
+    const gildedRose = new Shop(shopItems);
     const items = gildedRose.updateQuality();
     expect(items[0].name).to.equal("foo");
   });
