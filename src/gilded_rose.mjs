b8438d3aa77bb62212a26530d2b28e3074ac42a4
Rafael <rafaelrojas36@outlook.com>
2025-09-13 20:13:49 -0400

    r chocolateSauce to make it more readable

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -11,25 +11,25 @@ export class Shop {
     this.items = items;
   }
 
-  chocolateSauce(i) {
-    if (this.items[i].name != "Aged Brie" && this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-      if (this.items[i].quality > 0) {
-        if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-          this.items[i].quality = this.items[i].quality - 1;
+  chocolateSauce(item) {
+    if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
+      if (item.quality > 0) {
+        if (item.name != "Sulfuras, Hand of Ragnaros") {
+          item.quality = item.quality - 1;
         }
       }
     } else {
-      if (this.items[i].quality < 50) {
-        this.items[i].quality = this.items[i].quality + 1;
-        if (this.items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-          if (this.items[i].sellIn < 11) {
-            if (this.items[i].quality < 50) {
-              this.items[i].quality = this.items[i].quality + 1;
+      if (item.quality < 50) {
+        item.quality = item.quality + 1;
+        if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+          if (item.sellIn < 11) {
+            if (item.quality < 50) {
+              item.quality = item.quality + 1;
             }
           }
-          if (this.items[i].sellIn < 6) {
-            if (this.items[i].quality < 50) {
-              this.items[i].quality = this.items[i].quality + 1;
+          if (item.sellIn < 6) {
+            if (item.quality < 50) {
+              item.quality = item.quality + 1;
             }
           }
         }
@@ -65,7 +65,7 @@ export class Shop {
 
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
-      this.chocolateSauce(i);
+      this.chocolateSauce(this.items[i]);
       this.chocolateCream(i);
       this.chocolatePatatas(i);
     }
