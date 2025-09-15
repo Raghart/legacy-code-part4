fe6b7dcbb2df931dbf76081439bc3f91e8ece677
Rafael <rafaelrojas36@outlook.com>
2025-09-15 11:18:43 -0400

    tr adding logic to refactor test

--- test/gilded_rose.test.mjs
+++ test/gilded_rose.test.mjs
@@ -42,49 +42,15 @@ describe("Gilded Rose", () => {
   test("shop return expected possible paths for Backstage passes to a TAFKAL80ETC concert", () => {
     const items = [
       { name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 0, quality: 0 },
+      { name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 15, quality: 0 },
+      { name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 5, quality: 40 },
     ]
     const itemsToShop = items.map(item => new Item(item.name, item.sellIn, item.quality));
     const gildedRose = new Shop(itemsToShop);
     const qualityCheck = gildedRose.updateQuality();
     expect(qualityCheck).not.be.null.and.not.be.empty;
-    expect(qualityCheck.map(item => item.sellIn)).to.deep.equal([-1]);
-    expect(qualityCheck.map(item => item.quality)).to.deep.equal([0]);
-  });
-
-  test("shop returns Backstage passes to a TAFKAL80ETC concert", () => {
-    const name = "Backstage passes to a TAFKAL80ETC concert";
-    const sellIn = 0;
-    const quality = 0;
-    const gildedRose = new Shop([ new Item(name,sellIn,quality) ]);
-    const items = gildedRose.updateQuality();
-    expect(items).not.be.null.and.not.be.empty;
-    expect(items[0].name).to.equal.name;
-    expect(items[0].sellIn).to.equal(-1);
-    expect(items[0].quality).to.equal(0);
-  });
-
-  test("shop returns Backstage passes to a TAFKAL80ETC concert", () => {
-    const name = "Backstage passes to a TAFKAL80ETC concert";
-    const sellIn = 15;
-    const quality = 0;
-    const gildedRose = new Shop([ new Item(name,sellIn,quality) ]);
-    const items = gildedRose.updateQuality();
-    expect(items).not.be.null.and.not.be.empty;
-    expect(items[0].name).to.equal.name;
-    expect(items[0].sellIn).to.equal(14);
-    expect(items[0].quality).to.equal(1);
-  });
-
-  test("shop returns Backstage passes to a TAFKAL80ETC concert", () => {
-    const name = "Backstage passes to a TAFKAL80ETC concert";
-    const sellIn = 5;
-    const quality = 40;
-    const gildedRose = new Shop([ new Item(name,sellIn,quality) ]);
-    const items = gildedRose.updateQuality();
-    expect(items).not.be.null.and.not.be.empty;
-    expect(items[0].name).to.equal.name;
-    expect(items[0].sellIn).to.equal(4);
-    expect(items[0].quality).to.equal(43);
+    expect(qualityCheck.map(item => item.sellIn)).to.deep.equal([-1, 14, 4,]);
+    expect(qualityCheck.map(item => item.quality)).to.deep.equal([0, 1, 43]);
   });
 
   test("shop returns Aged Brie with big quality", () => {
