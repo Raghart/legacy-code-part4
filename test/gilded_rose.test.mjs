c3c63f1cd258915776b1b9177936cc4090247277
Rafael <rafaelrojas36@outlook.com>
2025-09-15 10:27:51 -0400

    t 4 tests

--- test/gilded_rose.test.mjs
+++ test/gilded_rose.test.mjs
@@ -38,4 +38,16 @@ describe("Gilded Rose", () => {
     expect(items[0].sellIn).to.equal(-2);
     expect(items[0].quality).to.equal(4);
   });
+
+  test("shop returns Backstage passes to a TAFKAL80ETC concert", () => {
+    const name = "Backstage passes to a TAFKAL80ETC concert";
+    const sellIn = 0;
+    const quality = 0;
+    const gildedRose = new Shop([ new Item(name,sellIn,quality) ]);
+    const items = gildedRose.updateQuality();
+    expect(items).not.be.null.and.not.be.empty;
+    expect(items[0].name).to.equal.name;
+    expect(items[0].sellIn).to.equal(-1);
+    expect(items[0].quality).to.equal(0);
+  });
 });
